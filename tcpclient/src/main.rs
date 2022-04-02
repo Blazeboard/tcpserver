a57a2b6f118652032c9947c7615114d4850d203a
Blazeboard <blazeboard@outlook.com>
2022-04-02 23:39:08 +0800

    add match

--- tcpclient/src/main.rs
+++ tcpclient/src/main.rs
@@ -4,17 +4,29 @@ use std::str;
 
 fn main() {
     // 与本地3000端口建立连接
-    let mut stream = TcpStream::connect("localhost:3000").unwrap();
+    let mut stream = match TcpStream::connect("localhost:3000") {
+        Ok(s) => s,
+        Err(e) => panic!("{}", e),
+    };
     // 向Steam中写入字符串Hello
-    stream.write("Hello".as_bytes()).unwrap();
+    match stream.write("Hello".as_bytes()) {
+        Ok(n) => println!("{}", n), // 向服务端发送的字符数
+        Err(e) => println!("{}", e),
+    };
 
     // 创建buffer以获取服务器发来的数据，这里只获取前4个字符
     let mut buffer = [0; 4];
-    stream.read(&mut buffer).unwrap();
+    match stream.read(&mut buffer) {
+        Ok(n) => println!("{}", n), // 从服务端接收到的字符数
+        Err(e) => println!("{}", e),
+    };
 
     // 将获取到的字符串打印出来
     println!(
-        "Response from server:{:?}",
-        str::from_utf8(&buffer).unwrap()
+        "收到服务端的回复:{:?}",
+        match str::from_utf8(&buffer) {
+            Ok(str) => str,
+            Err(e) => panic!("{}", e),
+        }
     );
 }
--- tcpserver/src/main.rs
+++ tcpserver/src/main.rs
@@ -3,19 +3,32 @@ use std::io::{ Read, Write };
 
 fn main() {
     // 建立TcpListener, 与本地的3000端口绑定
-    let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
-    println!("Running on port 3000...");
+    let listener = match TcpListener::bind("127.0.0.1:3000") {
+        Ok(l) => l,
+        Err(e) => panic!("{}", e),
+    };
+    println!("开始监听3000端口...");
 
     // 通过for循环，不断监听客户端的TCP请求
     for stream in listener.incoming() { // incoming返回一个TcpStream的迭代器
         // 使用同名变量（变量遮蔽）stream获取TcpStream, 建立连接
-        let mut stream = stream.unwrap();
-        println!("Connection established!");
+        let mut stream = match stream {
+            Ok(s) => s,
+            Err(e) => panic!("{}", e),
+        };
+        println!("连接建立!");
 
-        // 设置buffer用于接收客户端发来的数据
+         // 设置buffer用于接收客户端发来的数据，这里如果接收“hello”，
+         // 那么buffer中除了前五个位置外，其余位置都是零，buffer的总大小仍为1024
         let mut buffer = [0; 1024];
-        stream.read(&mut buffer).unwrap();
+        match stream.read(&mut buffer) {
+            Ok(n) => println!("客户端发送的字符数：{}", n), // 显示客户端发送过来的字符数
+            Err(e) => println!("发生错误：{}", e),
+        };
         // 将客户端发来的数据原样返回
-        stream.write(&mut buffer).unwrap();
+        match stream.write(&mut buffer) {
+            Ok(n) => println!("向客户端发送的字符数：{}", n), // 显示向客户端发送的字符数
+            Err(e) => println!("发生错误：{}", e),
+        };
     }
 }
